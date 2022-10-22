ab5486e0412bb7702b44eef4b318fa830ec69559
Léon Tiekötter <leon@tiekoetter.com>
2022-10-22 16:21:46 +0200

    Update update-hosts.sh

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,8 +1,8 @@
 google_dns_fix="`cat /etc/google-dns-fix/hosts`"
-hosts_tmpl="`tail -n 4 /etc/hosts`"
+hosts_tmpl="`tail -n 6 /etc/hosts`"
 if [ "$hosts_tmpl" != "$google_dns_fix" ]
 then
-	hosts="`head -n -4 /etc/hosts `"
+	hosts="`head -n -6 /etc/hosts `"
 	echo -e "$hosts\\n$google_dns_fix" > /etc/hosts
 else
 	echo "No update needed."
